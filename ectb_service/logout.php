41e2c7493d90995a5e904542b46376cff0fbbbed
xknolls <75128806+xknolls@users.noreply.github.com>
2021-06-25 08:37:27 +0200

    fghgf

--- ectb_service/logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once('inc/utils.php');
-init_session();
-clean_php_session();
-header('Location:index.php');
--- ectb_service/register_confirmed.php
+++
@@ -1,7 +0,0 @@
-<h1>Inscription réussite</h1>
-<p>Vous allez être redirigé vers la page de connexion d'ici quelques secondes.</p>
-<p>Si ce n'est pas le cas veuillez cliquer <a href="login.php">ici</a></p>
-
-<?php
-header("Refresh: 3;URL=login.php");
-?>
